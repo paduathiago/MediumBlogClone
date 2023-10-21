18d5e7da0c0162e2ec3c3c14bc5b6286fe1ab848
Thiago Padua <paduathiago@outlook.com>
2023-10-21 15:04:28 -0300

    client received first message from server

--- src/client.c
+++ src/client.c
@@ -8,10 +8,60 @@
 
 void process_server_op(struct BlogOperation op_received)
 {
-    if(op_received.operation_type == )
+    if(op_received.operation_type == NEW_CONNECION)
     {
 
     }
+    else if(op_received.operation_type == NEW_POST)
+    {
+
+    }
+    else if(op_received.operation_type == LIST_TOPICS)
+    {
+
+    }
+    else if(op_received.operation_type == SUBSCRIBE)
+    {
+
+    }
+    else if(op_received.operation_type == DISCONNECT)
+    {
+
+    }
+}
+
+struct BlogOperation process_input(char command[], const int id)
+{
+    struct BlogOperation op_sent;
+    op_sent.client_id = id;
+    op_sent.server_response = 0;
+    strcpy(op_sent.topic, "");
+
+    char *first_word;
+    first_word = strtok(command, " ");
+    
+    if(strcmp(first_word, "publish"))
+    {
+
+    }
+    else if(strcmp(first_word, "subscribe"))
+    {
+
+    }
+    else if(strcmp(first_word, "list"))
+    {
+
+    }
+    else if(strcmp(first_word, "disconnect"))
+    {
+
+    }
+    else
+    {
+        printf("Error! Invalid command\n");
+        exit(1);
+    }
+
 }
 
 int main(int argc, char *argv[])
@@ -33,23 +83,36 @@ int main(int argc, char *argv[])
     if(connect(sockfd, addr, sizeof(storage)) != 0)
         logexit("connect");
 
-    struct BlogOperation op_received;
+    struct BlogOperation op_sent, op_received;
+    op_sent.operation_type = NEW_CONNECION;
+    op_sent.client_id = 0;
+    op_sent.server_response = 0;
+    strcpy(op_sent.topic, "");
+    strcpy(op_sent.content, "");
+
+    size_t count_bytes_sent = send(sockfd, &op_sent, sizeof(struct BlogOperation), 0);
+    if(count_bytes_sent != sizeof(struct BlogOperation))
+        logexit("send");
+
+    receive_all(sockfd, &op_received, sizeof(struct BlogOperation)); // recv client's ID
+    int myid = op_received.client_id;
+
     while(1)
     {
         struct BlogOperation op_sent;
-        op_sent.type = -1;
+        op_sent.client_id = myid;
         
-        char op_topic[50];
-        char content[2048];
+        char command[100];
 
-        fgets(content, sizeof(content), stdin);
-        sscanf(content, );
+        fgets(command, sizeof(command), stdin);
+
+        // op_sent = process_input(op_topic, int id);
         
         size_t count_bytes_sent = send(sockfd, &op_sent, sizeof(struct BlogOperation), 0);
         if(count_bytes_sent != sizeof(struct BlogOperation))
             logexit("send");
         
-        if(op_sent.operation_type == )
+        if(op_sent.operation_type == DISCONNECT)
         {
             close(sockfd);
             break;
